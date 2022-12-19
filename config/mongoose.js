c673f423bcb6a7625e2543dca55fc43582cb0d94
anandkumarmca6 <anandkumarmca6@gmail.com>
2022-12-19 16:30:17 +0530

    set hosting

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/polling');
+mongoose.connect(
+  'mongodb://mongo:5F6rSINN5FZbkTlTEIdy@containers-us-west-171.railway.app:6916'
+);
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'Error connecting to mongodb'));
